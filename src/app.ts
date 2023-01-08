3da1672fc30638cdee96862e929fbd8e1d26bcf0
Willianson <williansondantas@gmail.com>
2023-01-08 15:14:56 -0300

    feat: insere middleware de erro e rotas

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
 import express from 'express';
 
+// código Willianson Dantas
+
+import ErrorHandler from './Middlewares/ErrorHandler';
+import routes from './Routes/Routes';
+
+// código Willianson Dantas
+
 const app = express();
+
+// código Willianson Dantas
+
+app.use(express.json());
+app.use(routes);
+app.use(ErrorHandler.handle);
+
+// código Willianson Dantas
 
 export default app;
