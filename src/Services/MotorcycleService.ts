56cb901b8bf2b84f10aa3b10141444bd90387a08
Willianson <williansondantas@gmail.com>
2023-01-08 18:02:24 -0300

    feat:req07, insere funções em service para o req07

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -10,12 +10,24 @@ export default class MotorcycleService {
         motorcycle,
       );
     }
-    throw new HttpException(404, 'Car not found');
+    throw new HttpException(404, 'Motorcycle not found');
   }
 
   public async motorcycleCreate(motorcycle: IMotorcycle) {
     const motorcycleODM = new MotorcycleODM();
     const newMotorcycle = await motorcycleODM.create(motorcycle);
     return this.createMotorcycleDomain(newMotorcycle);
+  }
+
+  public async motorcycleFindById(id: string) {
+    const motorcycleODM = new MotorcycleODM();
+    const motorcycleId = await motorcycleODM.findById(id);
+    return this.createMotorcycleDomain(motorcycleId);
+  }
+
+  public async motorcycleFind() {
+    const motorcycleODM = new MotorcycleODM();
+    const motorcycleList = await motorcycleODM.find();
+    return motorcycleList.map((motorcycle) => this.createMotorcycleDomain(motorcycle));
   }
 }
