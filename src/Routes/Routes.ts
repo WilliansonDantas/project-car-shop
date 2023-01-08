6755d156e8f41dcf2032950fc067a14001807a24
Willianson <williansondantas@gmail.com>
2023-01-08 17:19:29 -0300

    feat: req04, insere rota update

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -5,6 +5,8 @@ const routes = Router();
 
 routes.get('/cars/:id', (req, res, next) => new CarController(req, res, next).findById());
 
+routes.put('/cars/:id', (req, res, next) => new CarController(req, res, next).update());
+
 routes.post('/cars', (req, res, next) => new CarController(req, res, next).create());
 
 routes.get('/cars', (req, res, next) => new CarController(req, res, next).find());
