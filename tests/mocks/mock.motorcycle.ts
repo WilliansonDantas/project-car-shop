660382b24caedbb3aa773d86a69ad0e9990d05b2
Willianson <williansondantas@gmail.com>
2023-01-08 18:18:16 -0300

    feat: insere testes para motorcycle

---
+++ tests/mocks/mock.motorcycle.ts
@@ -0,0 +1,27 @@
+import IMotorcycle from '../../src/Interfaces/IMotorcycle';
+
+const Enter: IMotorcycle = {
+  model: 'Honda Cbr 1000rr',
+  year: 2011,
+  color: 'Orange',
+  status: true,
+  buyValue: 59.900,
+  category: 'Street',
+  engineCapacity: 1000,
+};
+
+const Out: IMotorcycle = {
+  id: '634852326b35b59438fbea31',
+  model: 'Honda Cbr 1000rr',
+  year: 2011,
+  color: 'Orange',
+  status: true,
+  buyValue: 59.900,
+  category: 'Street',
+  engineCapacity: 1000,
+};
+
+export {
+  Enter,
+  Out,
+};
