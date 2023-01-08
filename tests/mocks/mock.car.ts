f315d0a9e28e1a21416f4316fbc01fac63c212ee
Willianson <williansondantas@gmail.com>
2023-01-08 16:49:45 -0300

    feat: req03, insere mock para testes de car

---
+++ tests/mocks/mock.car.ts
@@ -0,0 +1,27 @@
+import ICar from '../../src/Interfaces/ICar';
+
+const Enter: ICar = {
+  model: 'Tempra',
+  year: 1995,
+  color: 'Black',
+  status: false,
+  buyValue: 39,
+  doorsQty: 2,
+  seatsQty: 5,
+};
+
+const Out: ICar = {
+  id: '634852326b35b59438fbea31',
+  model: 'Tempra',
+  year: 1995,
+  color: 'Black',
+  status: false,
+  buyValue: 39,
+  doorsQty: 2,
+  seatsQty: 5,
+};
+
+export {
+  Enter,
+  Out,
+};
